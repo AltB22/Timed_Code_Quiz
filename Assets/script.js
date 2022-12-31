129a9ac43f900efcc36a96773d144a6f6beb6fd6
Alt B 2022 <massie.w@gmail.com>
2022-12-30 23:43:49 -0800

    Re-configured approach to have all questions and answer buttons be dynamically created in JS.  Created startQuiz function and got it functioning.  Began const of quizQuestions as an array with an object inside equal to each question and set of possible answers.  Answers are an array [0-3].  Began function moveToQuestions.

---
+++ Assets/script.js
@@ -0,0 +1,37 @@
+// vars needed
+var startBtn = document.getElementById('#start-button');
+
+
+
+$(function startQuiz(){
+    $('#start-button').on('click', function(){
+    console.log('#start-button')
+    // startBtn.addEventListener('click',startQuiz);
+if ($('#start-button')){
+    console.log("hello");
+    // moveToQuestions();
+}}
+)
+}
+)   
+// });
+
+
+
+const quizQuestions = [
+    {
+        question: 'String values must be enclosed within___________when being assigned to variables.',
+        answerOptions: [
+            '1. Commas',
+            '2. Curly Brackets',
+            '3. Quotes',
+            '4. Parentheses',],
+            answer: 2,
+    }
+]
+
+function moveToQuestions(){
+
+}
+
+
