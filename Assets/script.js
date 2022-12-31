e9da9f55e7558841c29f20b2c65d0cc3bd3d4c56
Alt B 2022 <massie.w@gmail.com>
2022-12-31 11:02:40 -0800

    expanded questions and answers to 2. Created loop through the objects of Q/A array.  Likely need second for loop inside to reach the answer arrays.  Conosole logging questions and answers successfully in loop.

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,56 @@
 // vars needed
-var startBtn = document.getElementById('#start-button');
+// var startBtn = document.getElementById('#start-button');
 
 
 
 $(function startQuiz(){
     $('#start-button').on('click', function(){
-    console.log('#start-button')
-    // startBtn.addEventListener('click',startQuiz);
+
 if ($('#start-button')){
-    console.log("hello");
-    // moveToQuestions();
-}}
-)
+    moveToQuestions();
+    
 }
-)   
-// });
+});
+});
 
 
 
-const quizQuestions = [
+let quizQuestions = [
     {
         question: 'String values must be enclosed within___________when being assigned to variables.',
-        answerOptions: [
+        answers: [
             '1. Commas',
             '2. Curly Brackets',
             '3. Quotes',
             '4. Parentheses',],
             answer: 2,
+    },
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            '1. JavaScript',
+            '2. Terminal / Bash',
+            '3. A For Loop',
+            '4. Console.log()',],
+            answer: 3,
     }
 ]
 
 function moveToQuestions(){
 
+for (let i = 0; i <= quizQuestions.length; i++) {
+// console.log(quizQuestions.length)
+let questionObj = [i];
+if (questionObj[i] == quizQuestions.answer){
+    console.log(JSON.stringify(quizQuestions));
+
 }
 
+}
+// console.log(quizQuestions);
+//need a loop through the the questions array that returns each question/answer object as dynamically created p for quesiton and buttons in list for answers. Then moves to the next question/answer object when a selection is made.
 
+
+}
+
+// console.log(moveToQuestions);
