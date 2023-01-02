05fe5142694b485b4306fd3589070fc4001303e1
Alt B 2022 <massie.w@gmail.com>
2023-01-01 23:27:49 -0800

    created answer feedback function containing if statement to qualify answers

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,14 @@
-let xQuestion = 0;
 let startBtn = document.getElementById("start-button");
 let quizStart = document.getElementById('quiz-start');
 let startPageHeader = document.getElementById('start-page-header');
+let answerResult = document.getElementById('answer-result')
 let answerBtn; //answer buttons
 let currentQuestion; //question p
 let currentAnswers;
+let xQuestion = 0;
+let selectedAnswer = '';
 
-
-
-
-function startQuiz() {
-        if (startBtn) {
-            moveToQuestions();
-        }};
-
-startBtn.addEventListener('click', moveToQuestions);
-
-var questionAnswerObjArr = [
+let questionAnswerObjArr = [
     {
         question: 'String values must be enclosed within___________when being assigned to variables.',
         answers: [
@@ -24,7 +16,7 @@ var questionAnswerObjArr = [
             '2. Curly Brackets',
             '3. Quotes',
             '4. Parentheses',],
-        answer: 2,
+        answer: '3. Quotes',
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
@@ -33,9 +25,18 @@ var questionAnswerObjArr = [
             '2. Terminal / Bash',
             '3. A For Loop',
             '4. Console.log()',],
-        answer: 3,
+        answer: '4. Console.log()',
     },
 ]
+
+function startQuiz() {
+        if (startBtn) {
+            moveToQuestions();
+        }};
+
+startBtn.addEventListener('click', moveToQuestions);
+
+
 
 
 
@@ -45,32 +46,49 @@ function moveToQuestions() {
     startPageHeader.remove();
 
     var questionContainerEl = document.getElementById("question-container-id");
-   
     var listOfAnswers = document.createElement('ol');
     var questionText = document.createElement('p'); 
-
-
-    //need a loop through the the questions array that returns each question/answer object as dynamically created p for quesiton and buttons in list for answers. Then moves to the next question/answer object when a selection is made.
+    
+    // loops through the the questionsAnswerObjArr and returns each question/answer object as dynamically created p for question and buttons in list for answers
     for (i = 0; i < questionAnswerObjArr[xQuestion].answers.length; i++) {
         answerBtn = document.createElement('button');
         answerBtn.textContent = questionAnswerObjArr[xQuestion].answers[i];
+        
         questionText.textContent = questionAnswerObjArr[xQuestion].question;
         currentQuestion = questionText;
         currentAnswers = listOfAnswers;
         answerBtn.addEventListener('click', handleSelectedAnswer);
-        
+        // answerBtn.addEventListener('click', answerFeedback)
         listOfAnswers.append(answerBtn);
         questionContainerEl.append(questionText);
         questionContainerEl.append(listOfAnswers);
+
+        
     }
+   
 }
+
+// moves to the next question/answer object when a selection is made
 function handleSelectedAnswer() {
     currentAnswers.remove();
     currentQuestion.remove();
     xQuestion++
-
+    answerFeedback();
     moveToQuestions();
-}
+};
+
+
+function answerFeedback(){
+    selectedAnswer = answerBtn.textContent
+    if (selectedAnswer){
+        // console.log('Correct')
+        console.log(selectedAnswer)
+    }
+   
+//     if (selectedAnswer == questionAnswerObjArr.answer){
+//     console.log(questionAnswerObjArr[xQuestion].answer)
+// }
+};
 
 
   // for (let i = 0; i <= questionAnswerObjArr.length; i++) {
