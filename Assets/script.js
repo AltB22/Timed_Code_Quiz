7d56f2e804543659e7a6343179a4f289471042bd
Alt B 2022 <massie.w@gmail.com>
2023-01-05 15:59:53 -0800

    Went through entire JS and wrote comments for lets and functions.  Cleaned up any unncessary code

--- Assets/script.js
+++ Assets/script.js
@@ -11,34 +11,34 @@
 // THEN I can save my initials and score
 let mainContent = document.querySelector('.content-container')
 let contentEl = document.querySelector(".content") //class of content for the major content section elements
-let startBtn = document.getElementById("start-button"); //
-let quizStart = document.getElementById('quiz-start');
-let quizHomeEl = document.querySelector('.quiz-home')
-let startPageHeader = document.getElementById('start-page-header');
-let answerResult = document.getElementById('answer-result')
-let answerBtn ;//answer buttons
-let currentQuestion; //question p
-let currentAnswers;
-let xQuestion = 0;
-let timeOnClock; //amount of time to start
-timeOnClock = 60
+let startBtn = document.getElementById("start-button"); //start quiz button
+let quizStart = document.getElementById('quiz-start'); //p element for quiz start prompt
+let quizHomeEl = document.querySelector('.quiz-home') //div housing all child elements of quiz home class
+let startPageHeader = document.getElementById('start-page-header');//h1 starting header
+let answerResult = document.getElementById('answer-result')//section displaying feedback on answer to previous question.
+let answerBtn ; //answer button elements
+let currentQuestion; // p element housing each question text
+let currentAnswers; //global variable to house the current list of answer options
+let xQuestion = 0; //starting question is first index of the questionAnswerObjArr (I had to do this to give it starting value otherwise)
+let timeOnClock; //time remaining on clock (x)
+timeOnClock = 60 //amount of time to start
 let timeEl = document.getElementById('time-id');//span to house ticking time
-timeEl.textContent = timeOnClock
+timeEl.textContent = timeOnClock; //
 let timeInt = ""; //time interval (1 sec or 1000ms)
 let score = 0; //starting score
 let scoreEl = document.querySelector('.user-score-class');//span element to house score
 scoreEl.textContent = score + "%"; //score to be displayed
-let scoreInt = 10; //Amount added to score for each correct answer
+let scoreInt = 20; //Amount added to score for each correct answer
 let quizEnd = document.querySelector('.quiz-end'); //div class of "quiz-end"
-quizEnd.remove();
+quizEnd.remove();//Removes quiz by default until called in endQuiz function
 let userScoreEl = document.getElementById('high-scores');
-userScoreEl.remove();
-let scoresPage = document.querySelector(".scores-page");
-let userScores = document.querySelector("#user-score-id");
-var quizHome = document.createElement('button');
-quizHome.textContent = 'Home';
+userScoreEl.remove(); //Removes userScoreEl by default until called in saveScore => loop function
+let scoresPage = document.querySelector(".scores-page"); //div with clas scores-page
+// let userScores = document.querySelector("#user-score-id"); deprecated
+let quizHome = document.createElement('button'); //button to refresh quiz at end
+quizHome.textContent = 'Home'; //text content of quizHome
 
-
+//Array of objects containgin questions and their respective answers
 let questionAnswerObjArr = [
     {
         question: 'String values must be enclosed within___________when being assigned to variables.',
@@ -85,58 +85,13 @@ let questionAnswerObjArr = [
             'Global',],
         answer: 'Global',
     },
-    // {
-    //     question: 'An object data type can contain:',
-    //     answers: [
-    //         'An object',
-    //         'An array',
-    //         'A date',
-    //         'All of the Above',],
-    //     answer: 'All of the Above',
-    // },
-    // {
-    //     question: 'An object data type can contain:',
-    //     answers: [
-    //         'An object',
-    //         'An array',
-    //         'A date',
-    //         'All of the Above',],
-    //     answer: 'All of the Above',
-    // },
-    // {
-    //     question: 'An object data type can contain:',
-    //     answers: [
-    //         'An object',
-    //         'An array',
-    //         'A date',
-    //         'All of the Above',],
-    //     answer: 'All of the Above',
-    // },
-    // {
-    //     question: 'An object data type can contain:',
-    //     answers: [
-    //         'An object',
-    //         'An array',
-    //         'A date',
-    //         'All of the Above',],
-    //     answer: 'All of the Above',
-    // },
-    // {
-    //     question: 'An object data type can contain:',
-    //     answers: [
-    //         'An object',
-    //         'An array',
-    //         'A date',
-    //         'All of the Above',],
-    //     answer: 'All of the Above',
-    // },
 ]
-
+//sets clock interval to 1 sec
 function startQuiz(){
     timeInt = setInterval(startClock,1000);
-    
 }
 
+//starts the clock ticking down and displays.  If time at or less than zero (due to incorrect penalty) then stop function and move to endQuiz
 function startClock() {
     
     timeOnClock--;
@@ -147,6 +102,7 @@ function startClock() {
     }
 }
 
+//stops the clock, removes currentAnswer and current question. Displays quizEnd element and adds event listener to submit button
 function endQuiz(){
         clearInterval(timeInt);
         currentAnswers.remove();
@@ -156,27 +112,25 @@ function endQuiz(){
        
         let submitScore = document.querySelector(".submit-button");
         submitScore.addEventListener('click', saveScore);
-        
-        
 }
 
+//if saveScore is true then grab the value of the input form and set to local storage with initials as key and score as value.
 function saveScore(event) {
     event.preventDefault();
     
     let saveScore = document.querySelector(".submit-button");
    
     if (saveScore) {
-       
-        // var clearScores = document.createElement('button');
-        // clearScores.textContent = "Clear Scores"
     
         var key = document.querySelector('#initials').value;
         localStorage.setItem(key,score)
 
+        //shows "High Scores: Heading"
         var HighScoreText = document.createElement('h2');
         HighScoreText.textContent = 'High Scores:'
         scoresPage.append(HighScoreText);
 
+        //Loops through each key and creates elements for each score then appends in sequence and removes previous content
         Object.keys(localStorage).forEach((key) => {
             var highScoresList = document.createElement('ol');
 
@@ -194,23 +148,22 @@ function saveScore(event) {
             highScoresList.append(savedScoreList);
             scoresPage.append(userScoreEl);
             userScoreEl.append(userScoreId,insertBreak);
+
             quizEnd.remove();
             answerResult.remove();
-
-            userScoreId.textContent = key + " " + score;
+            //text to be displayed in list of scores
+            userScoreId.textContent = key + " " + score + "%";
         })
-        
         scoresPage.append(quizHome);
     }
 }
-
+//Refreshes quiz using Home button at end
 function refreshQuiz() {
    location.reload();
-        
     }
 
 
-
+//removes quiz home features and creates list of answers with text
 function moveToQuestions() {
     startBtn.remove();
     quizStart.remove();
@@ -219,9 +172,10 @@ function moveToQuestions() {
     var questionContainerEl = document.getElementById("question-container-id");
     var listOfAnswers = document.createElement('ol');
     var questionText = document.createElement('p');
-    
+
+    //if current question index postion is equal in type and value to the length of the questions array then stop function and move to end quiz
     if (xQuestion === questionAnswerObjArr.length){
-        questionAnswerObjArr.answers = ["done"];
+        questionAnswerObjArr.answers = ["done"];//I had to do this otherwise null error
      return endQuiz();
     }
 
@@ -242,7 +196,7 @@ function moveToQuestions() {
     }
 }
 
-// moves to the next question/answer object when a selection is made
+// moves to the next question/answer object when a selection is made and deducts 10 seconds from clock for each incorrect answers.  Renders text content feedback on selected answers.
 function handleSelectedAnswer() {
     if (this.innerHTML == questionAnswerObjArr[xQuestion].answer) {
         answerResult.textContent = 'Correct!'
@@ -262,7 +216,7 @@ function handleSelectedAnswer() {
     xQuestion++
     moveToQuestions();
 }
-      
+//buttons to call functions
 quizHome.addEventListener('click',refreshQuiz);
 startBtn.addEventListener('click', startQuiz);
 startBtn.addEventListener('click', moveToQuestions);
